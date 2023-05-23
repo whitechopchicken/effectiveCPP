cb5423bdbe74f319fa5ec9818160ab8fbff0477c
whitechopchicken <1249636417@qq.com>
2023-05-23 23:25:25 +0800

    week04

--- week04/examples/stdstring.cpp
+++ week04/examples/stdstring.cpp
@@ -7,6 +7,16 @@ int main()
     std::string str1 = "Hello";
     std::string str2 = "SUSTech";
     std::string result = str1 + ", " + str2;
+    std::u16string  u16str = u"this is a u16 string";
+    std::wstring wstr = L"this is a wstring ";
+    std::u32string u32str = U"this is a u32 string";
+
+    cout<<"wstring the sizeof  : "<<sizeof(wstring) <<endl; 
+    cout<<"u16 the sizeof  : "<<sizeof(u16string) <<endl; 
+    cout<<"u32 the sizeof  : "<<sizeof(u32string) <<endl;
+    
+    cout<<"u16 the length : "<<u16str.length() <<endl; 
+
 
     cout << "result = " + result << endl;
 
